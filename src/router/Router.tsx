1930bc2c344bcbd55adb5e207a537f37b1d7bab7
Vitalii S. <vitalii.sapunov@valsydev.com>
2025-12-30 20:54:35 +0200

    setup aliaces

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,10 +1,11 @@
 import { createBrowserRouter, type RouteObject } from "react-router-dom";
-import HomePage from "@/pages/HomePage";
+
 import NewsItemPage from "@/pages/NewsItemPage";
+import HomePage from "@/pages/HomePage";
 
 const routes: RouteObject[] = [
-  { path: "/", element: <HomePage /> },
-  { path: "/news/:id", element: <NewsItemPage /> },
+    { path: "/", element: <HomePage /> },
+    { path: "/news/:id", element: <NewsItemPage /> },
 ];
 
 const router = createBrowserRouter(routes);
