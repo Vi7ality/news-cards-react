8e3567d5a4e8d24fa2e5abb1f821b59097472a2b
Vitalii S. <vitalii.sapunov@valsydev.com>
2025-12-30 20:29:49 +0200

    created main pages

---
+++ src/pages/NewsItemPage/index.tsx
@@ -0,0 +1,5 @@
+const NewsItemPage = () => {
+  return <div>News Page</div>;
+};
+
+export default NewsItemPage;
--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,6 +1,11 @@
 import { createBrowserRouter, type RouteObject } from "react-router-dom";
+import HomePage from "@/pages/HomePage";
+import NewsItemPage from "@/pages/NewsItemPage";
 
-const routes: RouteObject[] = [{ path: "/", element: <div>Home</div> }];
+const routes: RouteObject[] = [
+  { path: "/", element: <HomePage /> },
+  { path: "/news/:id", element: <NewsItemPage /> },
+];
 
 const router = createBrowserRouter(routes);
 
