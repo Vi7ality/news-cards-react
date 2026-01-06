4e6963226f84a6a9e5c95e94a03a5e601c5235fd
Vitalii S. <vitalii.sapunov@valsydev.com>
2026-01-06 15:51:38 +0200

    theme provider added

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,18 @@ import "./index.css";
 import App from "./App.tsx";
 import { Provider } from "react-redux";
 import { store } from "./store/index.ts";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+const theme = createTheme();
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <Provider store={store}>
-            <App />
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <App />
+            </ThemeProvider>
         </Provider>
     </StrictMode>
 );
